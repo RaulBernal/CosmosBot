a9675302ceb595d3a15aa44c02879967cb30ed44
Raul Bernal <bernal.raul@gmail.com>
2021-01-08 10:54:57 +0100

    config file

---
+++ config.py
@@ -0,0 +1,12 @@
+#CONFIG THE BOT / CHAIN:  put a space after the path 
+path_to_daemon = '/usr/local/bin/bcnad '  #  Put the path to the daemon, leave a blank space 
+url_api = 'http://localhost:1317/'   # Url to API
+bcna_address='bcna1dpa0fkmjf3taqnc4fu43l2lgektt3caey6sf5j'   #Main address
+operator_address='bcnavaloper1dpa0fkmjf3taqnc4fu43l2lgektt3caea8pfwx' #Bcnad Validator address
+chain_id='bitcanna'
+priv_key='----'  #privkey of the wallet 
+wallet_name='Pandora' 
+url_explorer='http://testnet.cubital.es:8080/' + bandvaloper_address
+
+#About Telegram API with Botogram - create with BotFather bot
+token = '119870800:AAGSqBiz0_5LarTTJbV7soNHzGEedXlfggM' #  Put your own token
